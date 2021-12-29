97da199c2cd312ec7a4306ae8ef6a29e9429e1ff
hilaklein <63915907+hilaklein@users.noreply.github.com>
2021-12-29 22:14:11 +0200

    almost finish

--- Server/server2.py
+++ Server/server2.py
@@ -3,7 +3,6 @@ import struct
 from threading import *
 import random
 import time
-import scapy.arch
 
 UDP_DEST_PORT = 13117
 MAGIC_COOKIE = 0xabcddcba
@@ -34,9 +33,10 @@ class Server:
         self.server_udp_socket.bind(('', UDP_DEST_PORT))
         self.client_answer = None
         self.winning_mess = ""
-
+        self.answer = 0
         self.offer_thread = None
         self.recieve_thread = None
+        self.server_tcp_socket.bind(('', my_tcp_port))
 
     def tear_down(self):
         self.is2connected = False
@@ -46,6 +46,15 @@ class Server:
         self.games_threads = []
         self.numConnected = 0
         self.is_answered = False
+        # self.server_udp_socket = None
+        # self.server_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # udp socket
+        # self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # enable reuse address
+        # self.server_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # enable broatcat
+        # self.server_udp_socket.bind(('',UDP_DEST_PORT))
+        self.server_tcp_socket = None
+        self.server_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # tcp socket
+        self.server_tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_tcp_socket.bind(('', my_tcp_port))
         # self.offer_thread = None
         # self.recieve_thread = None
 
@@ -53,7 +62,7 @@ class Server:
 
         try:
             print("Server started, listening on IP address ", self.local_udp_ip) #change
-            self.server_tcp_socket.bind(('', my_tcp_port))
+
             offer_thread = Thread(target=self.broadcast_offers, daemon=True)
             offer_thread.start()
             recieve_thread = Thread(target=self.recieve_clients,daemon=True)
@@ -67,13 +76,8 @@ class Server:
     def generate_math_question(self):
         a = random.randint(0,9)
         b = random.randint(0,(9-a))
+        self.answer = a+b
         return [a,b]
-    # def end_game(self,team):
-    #     if self.is_answered:
-    #         return
-    #     else :
-    #         self.is_answered = True
-    #         self.winner = "the winner is " + team
 
 
     def broadcast_offers(self):
@@ -82,7 +86,7 @@ class Server:
                 time.sleep(1)
 
     def recieve_clients(self):
-        self.server_tcp_socket.listen(2)
+        self.server_tcp_socket.listen()
         while True:
             (conn, (dest_ip, dest_port)) = self.server_tcp_socket.accept()
             data = conn.recv(MESSAGE_SIZE).decode()  # get team name
@@ -91,68 +95,62 @@ class Server:
             self.games_threads.append(game_thread)
             self.numConnected = self.numConnected + 1
             if self.numConnected == 2:
+                begin_thread = Thread(target=self.begin_game_threads,daemon=True)
                 self.is2connected = False
-                time.sleep(4)  # wait 10 sec before starting the game
-                for t in self.games_threads:
-                    t.start()
-                for t in self.games_threads:
-                    t.join()
-                time.sleep(12)#wait 10 sec until game stop
-                # if self.is_answered == False:
-                #     answer = self.random_nums[0] + self.random_nums[1]
-                #     winning_mess = "Game Over!\n"+"The correct answer was "+ str(answer)+"!!!\n"+"No team answered within 10 sec.\n"+"It's a Draw!" +self.team_names[0][0]+ "and " +self.team_names[1][0]+ ", Try better next time :)\n"
-                #     conn_1 = self.team_names[0][1]
-                #     conn_2 = self.team_names[1][1]
-                #     conn_1.send(winning_mess.encode())
-                #     conn_2.send(winning_mess.encode())
-                #     conn_1.shutdown(socket.SHUT_RD)
-                #     conn_1.close()
-                #     conn_2.shutdown(socket.SHUT_RD)
-                #     conn_2.close()
+                begin_thread.start()
+                begin_thread.join()
+
                 # for t in self.games_threads:
-                #         t._Thread_stop()
-                # self.answering_lock.release()
+                #     t.join()
+                if self.is_answered == False:
+                    self.end_game()
+                self.team_names[0][1].send(self.winning_mess.encode())
+                self.team_names[1][1].send(self.winning_mess.encode())
+                self.team_names[0][1].close()
+                self.team_names[1][1].close()
+
                 print("Game over, sending out offer requests...")
                 break
         self.tear_down()
         self.Waiting_for_clients()
-
-
+    def begin_game_threads(self):
+        for t in self.games_threads:
+            t.start()
+        time.sleep(10)
+    def end_game(self):
+        answer = self.random_nums[0] + self.random_nums[1]
+        self.winning_mess = "Game Over!\n" + "The correct answer was " + str(answer) + "!!!\n" + "No team answered within 10 sec.\n" + "It's a Draw!" + self.team_names[0][
+                                0] + "and " + self.team_names[1][0] + ", Try better next time :)\n"
     def game_mode(self,conn,team):
+        # time.sleep(5)
         welcome_message = "Welcome to Quick Maths\n"+ "Player 1: "+ self.team_names[0][0]+ "Player 2: "+self.team_names[1][0]+ "\n=="
         answer = self.random_nums[0] + self.random_nums[1]
         math_question_message = "How much is "+ str(self.random_nums[0])+ "+"+ str(self.random_nums[1])
         conn.send(welcome_message.encode())
         conn.send(math_question_message.encode())
-        # conn.settimeout(10.0)
         try:
-            self.client_answer = conn.recv(MESSAGE_SIZE).decode()  # get answer
+            client_answer = conn.recv(MESSAGE_SIZE).decode()
+            self.client_answer = client_answer
+            print(client_answer)  # get answer
+        except Exception as ex:
+            pass
+        self.answering_lock.acquire()
+        if(self.is_answered == False):
 
-            self.answering_lock.acquire()
-            if (self.client_answer == None) == False: #you answer
+            if int(self.client_answer) == answer: #i won
+                print("win")
+                self.winner = team
+                self.is_answered = True
+            else:
+                print("bad")
+                self.is_answered = True
+                if(self.team_names[0] == team): #i lost - technical winning for other player
+                    self.winner = self.team_names[1]
+                else:
+                    self.winner = self.team_names[0]
+            self.winning_mess = "Game Over!"+"The correct answer was " + str(answer) + "!!!\n"+"Congratulations to the winner: " + self.winner
 
-                if (self.is_answered == False):
-
-                    if int(self.client_answer) == answer: #i won
-                        self.is_answered = True
-                        self.winner = team
-                    else:
-                        self.is_answered = True
-                        if(self.team_names[0] == team): #i lost - technical winning for other player
-                            self.winner = self.team_names[1]
-                        else:
-                            self.winner = self.team_names[0]
-                    self.winning_mess = "Game Over!\n"+"The correct answer was "+ str(answer)+"!!!\n"+"Congratulations to the winner: "+self.winner
-                    conn.send(self.winning_mess.encode())
-                    self.answering_lock.release()
-
-        except socket.timeout as ex:
-            self.winning_mess = "Game Over!\n" + "The correct answer was " + str(
-                answer) + "!!!\n" + "No team answered within 10 sec.\n" + "It's a Draw!" + self.team_names[0][
-                               0] + "and " + self.team_names[1][0] + ", Try better next time :)\n"
-
-        conn.shutdown(socket.SHUT_RD)
-        conn.close()
+            self.answering_lock.release()
 
 
 
